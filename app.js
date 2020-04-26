f8abde28bac6e43e101641fc7367abf2d8bdcf3d
Mwansa <Mwansad.mwango@gmail.com>
2020-04-26 13:55:40 +0800

    Added render function app.js

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+
 const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
@@ -14,6 +17,11 @@ const render = require("./lib/htmlRenderer");
 function promptEmployeeData() {
 
     return inquirer.prompt ([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?",
+        },
         {
             type: "input",
             name: "id",
@@ -35,11 +43,11 @@ function promptEmployeeData() {
             name: "github",
             message: "What is your github username?",
             when: function (answers) {
-                    if (answers.role === "Engineer") {
-                        return true;
-                    } else {
-                        return false;
-                    }
+                if (answers.role === "Engineer") {
+                    return true;
+                } else {
+                    return false;
+                }
             } 
         },
         {
@@ -47,11 +55,11 @@ function promptEmployeeData() {
             name: "school",
             message: "What school do you go to?",
             when: function (answers) {
-                    if (answers.role === "Intern") {
-                        return true;
-                    } else {
-                        return false;
-                    }
+                if (answers.role === "Intern") {
+                    return true;
+                } else {
+                    return false;
+                }
             } 
         },
         {
@@ -59,11 +67,11 @@ function promptEmployeeData() {
             name: "officeNumber",
             message: "What is your office number?",
             when: function (answers) {
-                    if (answers.role === "Manager") {
-                        return true;
-                    } else {
-                        return false;
-                    }
+                if (answers.role === "Manager") {
+                    return true;
+                } else {
+                    return false;
+                }
             } 
         },
         {
@@ -78,21 +86,59 @@ function promptEmployeeData() {
 async function getEmployeesData() {
 
     try {
-      // Get information about employees from the user
-      let employeeData = "";
-      let collectData = "";
-      do {
-          employeeData = await promptEmployeeData(); // initialization for first employee (at least 1)
-          collectData = employeeData.askAddEmployee;
-          console.log (collectData);
-                // if (role === 'Intern') {
-        //     let employee = new Intern(employeeData.name, employeeData.id, employeeData.email, employeeData.school)
-        // }
-      }
-      while (collectData); // Loop if collectData is true i.e. askAddEmployee answer is YES
+        // Get information about employees from the user
+        let employeeData = {};
+        let collectData = "";
+        let employeeList = [];
+        do {
+            employeeData = await promptEmployeeData(); // initialization for first employee (at least 1)
+            collectData = employeeData.askAddEmployee;
+            console.log (collectData);
+            // and to create objects for each team member (using the correct classes as blueprints!)
+            if (employeeData.role === 'Intern') {
+                let employee = new Intern(
+                    employeeData.name,
+                    employeeData.id,
+                    employeeData.email,
+                    employeeData.school
+                );
+                employeeList.push(employee);
+            }
+        }
+        while (collectData); // Loop if collectData is true i.e. askAddEmployee answer is YES
+        console.log(employeeList);
+
+        // After the user has input all employees desired, call the `render` function (required
+        // above) and pass in an array containing all employee objects; the `render` function will
+        // generate and return a block of HTML including templated divs for each employee!
+       
+        const html = render(employeeList);
+        // After you have your html, you're now ready to create an HTML file using the HTML
+        // returned from the `render` function. Now write it to a file named `team.html` in the
+        // `output` folder. You can use the variable `outputPath` above target this location.
+        // Hint: you may need to check if the `output` folder exists and create it if it
+        // does not.
+
+        //Check if file exists
+        fs.existsSync(OUTPUT_DIR) || fs.mkdirSync(OUTPUT_DIR) 
+        writeFileAsync(outputPath, html, (err) => {
+            if (err) throw err;
+        });   
+
+        // HINT: each employee type (manager, engineer, or intern) has slightly different
+        // information; write your code to ask different questions via inquirer depending on
+        // employee type.
+        
+        // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+        // and Intern classes should all extend from a class named Employee; see the directions
+        // for further information. Be sure to test out each class and verify it generates an 
+        // object with the correct structure and methods. This structure will be crucial in order
+        // for the provided `render` function to work!``'
+
     } catch(err) {
-      console.error(err.message);
-    }}
+        console.error(err.message);
+    }
+}
 
 getEmployeesData();
 
@@ -100,83 +146,11 @@ getEmployeesData();
 //       const queryRepoUrl = `https://api.github.com/repos/${answers.githubUsername}/${answers.githubRepo}`;
 //       const response = await axios.get(queryRepoUrl);
 //       const githubRepoData = response.data;
-   
-   
+
+
 //       // Get user data from Github API
 //       const profPicUrl = githubRepoData.owner.avatar_url;
 //       const githubUrl = githubRepoData.owner.url;
       
-//       // Generate readme content data
-//       let readMeData = 
-//   `
-//   # ${answers.projectTitle} ![Github Update](${updatedBadgeUrl}) ![Github Forks](${forksBadgeUrl}) ![Github Forks](${starsBadgeUrl}) ![Github Forks](${openIssuesBadgeUrl})
-//   ## Description
-//   ${answers.description}
-  
-//   ---
-//   ## Table of Contents
-  
-//    * [Installation](#installation)
-//    * [Usage](#usage)
-//    * [Licence](#licence)
-//    * [Contributing](#contributing)
-//    * [Tests](#tests)
-//    * [Questions](#questions) 
-//   ---
-//   ## Installation
-//   ${answers.installation}
-//   ## Usage
-//   ${answers.usage}
-//   ## Licence
-//   ${answers.licence}
-//   ## Contribution
-//   ${answers.contribution}
-//   ## Tests
-//   ${answers.tests}
-  
-//   ---
-//   ## Have any questions?
-//   ![Github profile picture](${profPicUrl})
-//   * [Github Link](${githubUrl})
-//   * Email: ${answers.email}
-//   `;
-  
-//       //Write to new readme file
-//       writeFileAsync ('MyReadME.md', readMeData, (err) => {
-//       if (err) throw err;
-//       });
-//       console.log ("Successfully created 'MyReadME.md' file");  
-//       //Handle rejected promise
-//     } catch(err) {
-//       console.error(err.message);
-//     }
-//   }
-  
-//   generateReadMe();
-// const numberOfEmployee = await 
-// for (i = 0, i < prom  )
 
-// // Get Answers from User prompts
-// const answers = await promptUser();
 
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!``'
